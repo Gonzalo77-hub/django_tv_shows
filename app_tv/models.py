c544e7e928a94a17be1ce979dcdd4199282ffd5f
gonzalo <gonzaloibarra8@gmail.com>
2021-08-23 17:38:53 -0400

    ejercicio TV_SHOWS con validaciones

--- app_tv/models.py
+++ app_tv/models.py
@@ -1,12 +1,45 @@
 from django.db import models
 
+class ShowManager(models.Manager):
+
+    def basic_validator(self, postData):
+        errors = {}
+        print(postData['title'])
+        # agregue claves y valores al diccionario de errores para cada campo no válido
+        if len(postData['title']) < 2:
+            errors["title"] = "El largo del titulo debe ser mayor a 2 caracteres"
+        if len(postData['Network']) < 3:
+            errors["Network"] = "El largo del network debe ser mayor a 3 caracteres"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "El largo de la descripcion debe ser mayor a 10 caracteres"
+        if Show.objects.filter(title=postData['title']).exists():
+            errors['existe'] ='El titulo ya existe, favor ingresar otro'
+        print(errors)
+        return errors
+    def edit_validator(self, postData):
+        errors = {}
+        print(postData['edit_title'])
+        # agregue claves y valores al diccionario de errores para cada campo no válido
+        if len(postData['edit_title']) < 2:
+            errors["ed_title"] = "El largo del titulo debe ser mayor a 2 caracteres"
+        if len(postData['edit_Network']) < 3:
+            errors["ed_Network"] = "El largo del network debe ser mayor a 3 caracteres"
+        if len(postData['edit_desc']) < 10:
+            errors["ed_desc"] = "El largo de la descripcion debe ser mayor a 10 caracteres"
+        if Show.objects.filter(title=postData['edit_title']).exclude(title=postData['edit_title']).exists():
+            errors['existe'] ='El titulo ya existe, favor ingresar otro'
+        print(errors)
+        print(errors)
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=25)
     desc = models.CharField(max_length=255)
     Release = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()  
 
     def __repr__(self):
         return f"Nombre Serie: {self.title}, plataforma: {self.network}"
--- app_tv/views.py
+++ app_tv/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import *
 
 
@@ -6,18 +7,29 @@ def redir(request):
     return redirect("/Shows")
 
 def crear(request):
-    titulo = request.POST['title']
-    network = request.POST['Network']
-    release = request.POST['Release']
-    description = request.POST['desc']
-    show = Show.objects.create(
-    title=titulo, 
-    network=network, 
-    Release=release,
-    desc= description,)
+
+    errors= Show.objects.basic_validator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect("/Shows/New")
+    else:
+
+        titulo = request.POST['title']
+        network = request.POST['Network']
+        release = request.POST['Release']
+        description = request.POST['desc']
+        show = Show.objects.create(
+        title=titulo, 
+        network=network, 
+        Release=release,
+        desc= description,)
+        messages.success(request, "Blog successfully updated")
 
 
-    return redirect(f"/Shows/{show.id}")
+        return redirect(f"/Shows/{show.id}")
 
 def index(request):
     context = {
@@ -42,18 +54,27 @@ def edit_show(request, val):
     return render(request, 'edit.html', variable)
 
 def edit(request):
-    ed_title = request.POST['edit_title']
-    ed_network =request.POST['edit_Network']
-    ed_release = request.POST['edit_releaseDate']
-    ed_desc = request.POST['edit_desc']
-    id_programa = request.POST['id_show']
-    program = Show.objects.get(id=id_programa)
-    program.title = ed_title
-    program.network = ed_network
-    program.Release = ed_release
-    program.desc = ed_desc
-    program.save()
-    return redirect("/Shows")
+    print(request.POST)
+    errors= Show.objects.edit_validator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect(f"/Shows/{request.POST['id_show']}/edit")
+    else:
+        ed_title = request.POST['edit_title']
+        ed_network =request.POST['edit_Network']
+        ed_release = request.POST['edit_releaseDate']
+        ed_desc = request.POST['edit_desc']
+        id_programa = request.POST['id_show']
+        program = Show.objects.get(id=id_programa)
+        program.title = ed_title
+        program.network = ed_network
+        program.Release = ed_release
+        program.desc = ed_desc
+        program.save()
+        return redirect("/Shows")
 def delete(request, val):
     borr = Show.objects.get(id=val)
     borr.delete()
